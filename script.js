fe68bfe317abfcbf1225ef08bff0b0e2b622a2f0
soufhmch <91191046+soufhmch@users.noreply.github.com>
2025-01-29 17:43:48 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+    ScrollReveal().reveal('.reveal', {
+        duration: 1000,
+        origin: 'bottom',
+        distance: '50px',
+        delay: 200,
+        easing: 'ease-in-out',
+    });
+});
